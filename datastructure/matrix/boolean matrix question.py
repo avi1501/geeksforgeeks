ad2c08494ba666e0cd48b0df7ae3e5513c92fa91
avi1501 <officialaviid@gmail.com>
2020-07-01 15:49:06 +0530

    .

---
+++ datastructure/matrix/boolean matrix question.py
@@ -0,0 +1,31 @@
+def boolmatrix(matrix):
+    m = len(matrix)
+    n = len(matrix[0])
+    row = [0]*m
+    col = [0]*n
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == 1:
+                row[i] = 1
+                col[j] = 1
+    for i in range(m):
+        for j in range(n):
+            if col[j] == 1 or row[i] == 1:
+                matrix[i][j] == 1
+            
+def printMatrix(matrix):
+    for rows in matrix:
+        print(rows)
+
+matrix = [
+            [1,0,0,1],
+            [0,0,1,0],
+            [0,0,0,0],
+            [0,0,0,0]
+         ]
+
+print('matrix before')
+printMatrix(matrix)
+boolmatrix(matrix)
+print('matrix after')
+printMatrix(matrix)
